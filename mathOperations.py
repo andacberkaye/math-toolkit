abb1b57e766a7fc527558ba3e3eb79df3d9a2d8b
Andaç Berkay Ekren <andacberkayekren@Andac-MacBook-Air.local>
2025-05-19 20:41:57 +0300

    Update subtraction function in mathOperations.py

--- mathOperations.py
+++ mathOperations.py
@@ -8,3 +8,17 @@ def Addition(*a):
         addition = addition + x
     
     return addition
+
+def Subtraction(*a):
+    
+    numbers = [i for i in a]
+     
+    subtraction = numbers[0]
+
+    for x in numbers:
+        if subtraction == x:
+            continue
+        else:
+            subtraction = subtraction - x
+    
+    return subtraction
