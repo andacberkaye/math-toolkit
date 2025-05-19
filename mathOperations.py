b14d904c2753409c61ab44f11b90babadc4094b0
Andaç Berkay Ekren <andacberkayekren@Andac-MacBook-Air.local>
2025-05-19 22:30:08 +0300

    Update exponents function in mathOperations.py

--- mathOperations.py
+++ mathOperations.py
@@ -34,7 +34,10 @@ def Multiplication(*a):
     
     return multiplication
 
-def Division(a,b=1):
-    if b == 0:
+def Division(a = 1,b=1):
+    if b == 0 or a == 0:
         return "Division by zero is not allowed."
     return a / b
+
+def Exponents(a = 1, b = 1):
+    return a**b
