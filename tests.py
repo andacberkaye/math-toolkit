6daac30df05b6c6b96ff755857c6b8ad56af889a
Andaç Berkay ekren <andacberkayekren@gmail.com>
2025-08-11 12:48:25 +0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -28,8 +28,8 @@ def run_tests():
     assert octal(10) == "12", "Octal failed"
     assert binary(5) == "101", "Binary failed"
     assert reverse_number(1234) == "4321", "Reverse number failed"
-    assert gcd(12, 18) == 6, "EBOB failed"
-    assert lcm(4, 6) == 12, "EKOK failed"
+    assert gcd(12, 18) == 6, "GCD failed"
+    assert lcm(4, 6) == 12, "LCM failed"
 
     print("✅ All tests passed successfully!")
 
